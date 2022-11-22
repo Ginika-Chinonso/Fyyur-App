87237e124898afdcdaaa0ef74bf3d2567b4d027a
rephanfrancis <ginika.chinonso.francis@gmail.com>
2022-11-22 12:29:28 +0100

    refactor: Update migration file

--- projects/01_fyyur/starter_code/migrations/versions/99ac0e4ac994_.py
+++ projects/01_fyyur/starter_code/migrations/versions/99ac0e4ac994_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: cce5f5ec98dc
+Revision ID: 99ac0e4ac994
 Revises: 
-Create Date: 2022-11-20 00:15:48.672229
+Create Date: 2022-11-22 00:56:09.046278
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'cce5f5ec98dc'
+revision = '99ac0e4ac994'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -24,7 +24,7 @@ def upgrade():
     sa.Column('city', sa.String(length=120), nullable=False),
     sa.Column('state', sa.String(length=120), nullable=False),
     sa.Column('phone', sa.String(length=120), nullable=True),
-    sa.Column('website', sa.String(), nullable=True),
+    sa.Column('website_link', sa.String(), nullable=True),
     sa.Column('genres', sa.ARRAY(sa.String()), nullable=False),
     sa.Column('image_link', sa.String(length=500), nullable=True),
     sa.Column('facebook_link', sa.String(length=120), nullable=True),
@@ -42,14 +42,14 @@ def upgrade():
     sa.Column('image_link', sa.String(length=500), nullable=True),
     sa.Column('facebook_link', sa.String(length=120), nullable=True),
     sa.Column('genres', sa.ARRAY(sa.String()), nullable=False),
-    sa.Column('website', sa.String(), nullable=True),
+    sa.Column('website_link', sa.String(), nullable=True),
     sa.Column('seeking_talent', sa.Boolean(), nullable=True),
     sa.Column('seeking_description', sa.String(), nullable=True),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('shows',
     sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('start_time', sa.String(), nullable=True),
+    sa.Column('start_time', sa.DateTime(), nullable=True),
     sa.Column('artist_id', sa.Integer(), nullable=False),
     sa.Column('venue_id', sa.Integer(), nullable=False),
     sa.ForeignKeyConstraint(['artist_id'], ['Artist.id'], ),
